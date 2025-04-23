f809f2ad382832680ffa3d2645fa1b8b5a5405e7
Ângelo <126002318+parrelladev@users.noreply.github.com>
2025-04-23 11:37:22 -0300

    Modelo de layout Vagas Internas

---
+++ packages/editor-sample/src/getConfiguration/sample/vagas-internas.ts
@@ -0,0 +1,516 @@
+import { TEditorConfiguration } from '../../documents/editor/core';
+
+const VAGAS_INTERNAS: TEditorConfiguration = {
+  root: {
+    type: 'EmailLayout',
+    data: {
+      backdropColor: '#FFFFFF',
+      borderRadius: 8,
+      canvasColor: '#FFFFFF',
+      textColor: '#262626',
+      fontFamily: 'MODERN_SANS',
+      childrenIds: [
+        'block-1726148704358',
+        'block-1726150166438',
+        'block-1726160372942',
+        'block-1726151384169',
+        'block-1726150716035',
+        'block-1726161479910',
+        'block-1726150863331',
+        'block-1726151832523',
+        'block-1726151000918'
+      ]
+    }
+  },
+"block-1726148704358": {
+    "type": "Image",
+    "data": {
+      "style": {
+        "padding": {
+          "top": 0,
+          "bottom": 0,
+          "right": 0,
+          "left": 0
+        },
+        "backgroundColor": null,
+        "textAlign": "center"
+      },
+      "props": {
+        "width": 320,
+        "height": null,
+        "url": "https://www.redegazeta.com.br/wp-content/uploads/2024/09/Vagas-Internas.png",
+        "alt": "Vagas Internas",
+        "linkHref": null,
+        "contentAlignment": "middle"
+      }
+    }
+  },
+  "block-1726150166438": {
+    "type": "Divider",
+    "data": {
+      "style": {
+        "padding": {
+          "top": 0,
+          "bottom": 0,
+          "right": 0,
+          "left": 0
+        }
+      },
+      "props": {
+        "lineColor": "#0891B2"
+      }
+    }
+  },
+  "block-1726150716035": {
+    "type": "Heading",
+    "data": {
+      "props": {
+        "text": "Como participar da vaga interna?\n",
+        "level": "h2"
+      },
+      "style": {
+        "color": "#FFFFFF",
+        "backgroundColor": "#0071c6",
+        "padding": {
+          "top": 16,
+          "bottom": 8,
+          "right": 32,
+          "left": 32
+        }
+      }
+    }
+  },
+  "block-1726150863331": {
+    "type": "Heading",
+    "data": {
+      "props": {
+        "text": "Entenda as etapas do processo seletivo:",
+        "level": "h3"
+      },
+      "style": {
+        "color": "#FFFFFF",
+        "backgroundColor": "#0071c6",
+        "padding": {
+          "top": 12,
+          "bottom": 12,
+          "right": 32,
+          "left": 32
+        }
+      }
+    }
+  },
+  "block-1726151000918": {
+    "type": "Text",
+    "data": {
+      "style": {
+        "color": "#FFFFFF",
+        "backgroundColor": "#0071c6",
+        "fontSize": 13,
+        "fontWeight": "normal",
+        "textAlign": "center",
+        "padding": {
+          "top": 16,
+          "bottom": 16,
+          "right": 32,
+          "left": 32
+        }
+      },
+      "props": {
+        "text": "Em caso de dúvidas, entre em contato com o ramal 8399."
+      }
+    }
+  },
+  "block-1726151384169": {
+    "type": "Divider",
+    "data": {
+      "style": {
+        "padding": {
+          "top": 0,
+          "bottom": 0,
+          "right": 0,
+          "left": 0
+        }
+      },
+      "props": {
+        "lineColor": "#0891B2",
+        "lineHeight": 2
+      }
+    }
+  },
+  "block-1726151832523": {
+    "type": "Image",
+    "data": {
+      "style": {
+        "padding": {
+          "top": 8,
+          "bottom": 8,
+          "right": 36,
+          "left": 36
+        },
+        "backgroundColor": "#0071c6",
+        "textAlign": "center"
+      },
+      "props": {
+        "width": 400,
+        "url": "https://www.redegazeta.com.br/wp-content/uploads/2024/09/Vagas-Internas-Processo-3.png",
+        "alt": "Organograma das etapas",
+        "linkHref": null,
+        "contentAlignment": "middle"
+      }
+    }
+  },
+  "block-1726152389246": {
+    "type": "Heading",
+    "data": {
+      "props": {
+        "text": "Confira os gazeters aprovados no processo interno",
+        "level": "h3"
+      },
+      "style": {
+        "color": "#FFFFFF",
+        "backgroundColor": "#0891B2",
+        "padding": {
+          "top": 16,
+          "bottom": 16,
+          "right": 24,
+          "left": 24
+        }
+      }
+    }
+  },
+  "block-1726152432817": {
+    "type": "ColumnsContainer",
+    "data": {
+      "style": {
+        "backgroundColor": "#0891B2",
+        "padding": {
+          "top": 8,
+          "bottom": 16,
+          "right": 0,
+          "left": 0
+        }
+      },
+      "props": {
+        "fixedWidths": [
+          80,
+          null,
+          null
+        ],
+        "columnsCount": 2,
+        "columnsGap": 4,
+        "columns": [
+          {
+            "childrenIds": [
+              "block-1726152434726"
+            ]
+          },
+          {
+            "childrenIds": [
+              "block-1726152636632",
+              "block-1726152442358"
+            ]
+          },
+          {
+            "childrenIds": []
+          }
+        ]
+      }
+    }
+  },
+  "block-1726152434726": {
+    "type": "Avatar",
+    "data": {
+      "style": {
+        "padding": {
+          "top": 0,
+          "bottom": 0,
+          "right": 0,
+          "left": 20
+        }
+      },
+      "props": {
+        "size": 65,
+        "shape": "circle",
+        "imageUrl": "https://ui-avatars.com/api/?size=128"
+      }
+    }
+  },
+  "block-1726152442358": {
+    "type": "Text",
+    "data": {
+      "style": {
+        "color": "#FFFFFF",
+        "fontSize": 14,
+        "fontWeight": "normal",
+        "padding": {
+          "top": 8,
+          "bottom": 8,
+          "right": 16,
+          "left": 16
+        }
+      },
+      "props": {
+        "text": "Era de tal cargo e agora está em tal cargo em X setor"
+      }
+    }
+  },
+  "block-1726152636632": {
+    "type": "Heading",
+    "data": {
+      "props": {
+        "text": "Fulano de tal",
+        "level": "h3"
+      },
+      "style": {
+        "color": "#FFFFFF",
+        "padding": {
+          "top": 0,
+          "bottom": 0,
+          "right": 16,
+          "left": 16
+        }
+      }
+    }
+  },
+  "block-1726160372942": {
+    "type": "ColumnsContainer",
+    "data": {
+      "style": {
+        "padding": {
+          "top": 8,
+          "bottom": 4,
+          "right": 24,
+          "left": 24
+        }
+      },
+      "props": {
+        "columnsCount": 2,
+        "columnsGap": 16,
+        "columns": [
+          {
+            "childrenIds": [
+              "block-1726160377054"
+            ]
+          },
+          {
+            "childrenIds": [
+              "block-1737550727020"
+            ]
+          },
+          {
+            "childrenIds": []
+          }
+        ]
+      }
+    }
+  },
+  "block-1726160377054": {
+    "type": "Html",
+    "data": {
+      "style": {
+        "color": null,
+        "backgroundColor": null,
+        "fontSize": 14,
+        "textAlign": "left",
+        "padding": {
+          "top": 16,
+          "bottom": 16,
+          "right": 24,
+          "left": 24
+        }
+      },
+      "props": {
+        "contents": "<b>Cargo: </b>Analista de Inteligência de Dados\n<br>\n<b>Setor: </b>Produção e Programação<br>\n<b>Local: </b>Vitória<br>\n<b>Inscrições na Gupy:</b> <a href=\"https://redegazeta.gupy.io/jobs/8475339\">Acessar</a><br>\n"
+      }
+    }
+  },
+  "block-1726161479910": {
+    "type": "ColumnsContainer",
+    "data": {
+      "style": {
+        "backgroundColor": "#0071c6",
+        "padding": {
+          "top": 16,
+          "bottom": 16,
+          "right": 24,
+          "left": 24
+        }
+      },
+      "props": {
+        "columnsCount": 3,
+        "columnsGap": 16,
+        "contentAlignment": "top",
+        "columns": [
+          {
+            "childrenIds": [
+              "block-1726161500997",
+              "block-1726161535309"
+            ]
+          },
+          {
+            "childrenIds": [
+              "block-1726161640629",
+              "block-1726161653824"
+            ]
+          },
+          {
+            "childrenIds": [
+              "block-1726161848613",
+              "block-1726161869870"
+            ]
+          }
+        ]
+      }
+    }
+  },
+  "block-1726161500997": {
+    "type": "Heading",
+    "data": {
+      "props": {
+        "text": "1º PASSO",
+        "level": "h3"
+      },
+      "style": {
+        "color": "#FFFFFF",
+        "textAlign": "left",
+        "padding": {
+          "top": 0,
+          "bottom": 0,
+          "right": 12,
+          "left": 12
+        }
+      }
+    }
+  },
+  "block-1726161535309": {
+    "type": "Text",
+    "data": {
+      "style": {
+        "color": "#FFFFFF",
+        "fontSize": 14,
+        "fontWeight": "normal",
+        "padding": {
+          "top": 0,
+          "bottom": 0,
+          "right": 12,
+          "left": 12
+        }
+      },
+      "props": {
+        "text": "Se inscreva na vaga pelo site da Gupy\n"
+      }
+    }
+  },
+  "block-1726161640629": {
+    "type": "Heading",
+    "data": {
+      "props": {
+        "text": "2º PASSO",
+        "level": "h3"
+      },
+      "style": {
+        "color": "#FFFFFF",
+        "padding": {
+          "top": 0,
+          "bottom": 0,
+          "right": 12,
+          "left": 12
+        }
+      }
+    }
+  },
+  "block-1726161653824": {
+    "type": "Text",
+    "data": {
+      "style": {
+        "color": "#FFFFFF",
+        "fontSize": 14,
+        "fontWeight": "normal",
+        "padding": {
+          "top": 0,
+          "bottom": 0,
+          "right": 12,
+          "left": 12
+        }
+      },
+      "props": {
+        "text": "Responda ao questionários sobre indicação para o cargo\n"
+      }
+    }
+  },
+  "block-1726161848613": {
+    "type": "Heading",
+    "data": {
+      "props": {
+        "text": "3 PASSO",
+        "level": "h3"
+      },
+      "style": {
+        "color": "#FFFFFF",
+        "padding": {
+          "top": 0,
+          "bottom": 0,
+          "right": 12,
+          "left": 12
+        }
+      }
+    }
+  },
+  "block-1726161869870": {
+    "type": "Text",
+    "data": {
+      "style": {
+        "color": "#FFFFFF",
+        "fontSize": 14,
+        "fontWeight": "normal",
+        "padding": {
+          "top": 0,
+          "bottom": 0,
+          "right": 12,
+          "left": 12
+        }
+      },
+      "props": {
+        "text": "Coloque o nome e e-mail corporativo do seu gestor\n"
+      }
+    }
+  },
+  "block-1726496962290": {
+    "type": "Avatar",
+    "data": {
+      "style": {
+        "padding": {
+          "top": 16,
+          "bottom": 16,
+          "right": 0,
+          "left": 0
+        }
+      },
+      "props": {
+        "size": 65,
+        "shape": "circle",
+        "imageUrl": "https://ui-avatars.com/api/?size=128"
+      }
+    }
+  },
+  "block-1737550727020": {
+    "type": "Html",
+    "data": {
+      "style": {
+        "fontSize": 14,
+        "textAlign": null,
+        "padding": {
+          "top": 16,
+          "bottom": 16,
+          "right": 24,
+          "left": 24
+        }
+      },
+      "props": {
+        "contents": "<b>Cargo: </b>Editor de Mídia Audiovisual\n<br>\n<b>Setor: </b>Produção e Programação<br>\n<b>Local: </b>Vitória<br>\n<b>Inscrições na Gupy:</b> <a href=\"https://redegazeta.gupy.io/jobs/8492575\">Acessar</a><br>\n"
+      }
+    }
+  }
+};
+
+export default VAGAS_INTERNAS;
